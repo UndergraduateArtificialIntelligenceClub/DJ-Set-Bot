bcf4f39d8b6e21ba15ba6947e507c9dd11602ffa
quintinh <47458953+qhansen628@users.noreply.github.com>
2024-02-09 16:49:49 -0700

    update readme
    preliminary instructions for local set up for feature generation

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 from essentia.standard import MonoLoader, TensorflowPredictMAEST
 
-audio = MonoLoader(filename="18025392_365_(Original Mix).mp3", sampleRate=32000, resampleQuality=4)()
-model = TensorflowPredictMAEST(graphFilename="essentiaModel/discogs-maest-30s-pw-1.pb", output="StatefulPartitionedCall:7")
-embeddings = model(audio)
-print(embeddings)
+audioFile = input("Enter path to mp3 (320kps quality):")
+try:
+    audio = MonoLoader(filename=audioFile, sampleRate=32000, resampleQuality=4)()
+except Exception as e:
+    print(e)
+else:
+    model = TensorflowPredictMAEST(graphFilename="essentiaModel/discogs-maest-30s-pw-1.pb", output="StatefulPartitionedCall:7")
+    embeddings = model(audio)
+    print(embeddings)
